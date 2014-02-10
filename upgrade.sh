a30e7d0e48abba679b01ee9e71b3123beb36a483
ArnaudParant <ard.parant@gmail.com>
2014-02-10 13:32:43 -0600

    Added wikidoc deps

--- upgrade.sh
+++ upgrade.sh
@@ -5,6 +5,9 @@ tar -xvf ocaml-data-notation.tar.gz && cd ocaml-data-notation-0.0.11 && ./config
 tar -xvf oasis.tar.gz && cd oasis-0.4.0 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/userconf && rm -rf ~/.opam/4.00.1/lib/oasis && rm -rf ~/.opam/4.00.1/lib/plugin-loader
 make install || sudo cp _build/src/cli/Main.byte /usr/local/bin/oasis && cd ..
 
+git clone https://github.com/ocsigen/wikidoc.git && cd wikidoc && make && sudo make uninstall && rm -rf ~/.opam/4.00.1/lib/wikidoc
+make install || sudo make install && cd ..
+
 echo "Y\n" | opam reinstall eliom
 
 git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
