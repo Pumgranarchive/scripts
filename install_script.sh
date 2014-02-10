775801270aaf36617decf2cabe1d56f59d9c0178
ArnaudParant <ard.parant@gmail.com>
2014-02-10 13:14:57 -0600

    Improve the stability of scripts

--- install_script.sh
+++ install_script.sh
@@ -5,23 +5,24 @@ echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' |
 sudo apt-get update
 sudo apt-get install libev-dev libgdbm-dev libncurses5-dev libpcre3-dev libssl-dev libsqlite3-dev libcairo-ocaml-dev m4 libev-dev ocaml ocaml-compiler-libs camlp4 camlp4-extra ocaml-findlib libfindlib-ocaml-dev libreact-ocaml-dev libssl-ocaml-dev libpcre-ocaml-dev libcryptokit-ocaml-dev libtext-ocaml-dev libcalendar-ocaml-dev libsqlite3-ocaml-dev libzip-ocaml-dev libtype-conv-camlp4-dev libpam0g-dev postgresql libextlib-ocaml-dev libcsv-ocaml-dev libpgocaml-ocaml-dev libpam-ocaml-dev mongodb-10gen git
 
-git clone https://github.com/ocaml/opam.git
-cd opam && ./configure && make && sudo make install && cd ..
+git clone https://github.com/ocaml/opam.git && cd opam && ./configure && make && sudo make install && cd ..
 
 echo "y\n" | opam init
 eval `opam config env`
 opam repository add opamocsigen http://ocsigen.org/opam
 opam switch 4.00.1
 eval `opam config env`
+
+tar -xvf ocaml-data-notation.tar.gz && cd ocaml-data-notation-0.0.11 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/odn && make install && cd ..
+
+tar -xvf oasis.tar.gz && cd oasis-0.4.0 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/userconf && rm -rf ~/.opam/4.00.1/lib/oasis && rm -rf ~/.opam/4.00.1/lib/plugin-loader
+
 echo "Y\n" | opam install dbm menhir eliom yojson
 
-git clone https://github.com/MassD/bson.git
-cd bson && ./configure && make && make install && cd ..
+git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
 
-git clone https://github.com/MassD/mongo.git
-cd mongo && ./configure && make && make install && cd ..
+git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
 
 sudo /etc/init.d/mongodb start
 
-git clone https://github.com/Pumgrana/prototype.git
-cd prototype && mongorestore export_db/pumgrana && make test.byte &
+git clone https://github.com/Pumgrana/prototype.git && cd prototype && mongorestore export_db/pumgrana && make test.byte &
--- update.sh
+++ update.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-tar -xvf ocaml-data-notation.tar.gz
-cd ocaml-data-notation && ./configure && make && sudo make install && cd ..
+tar -xvf ocaml-data-notation.tar.gz && cd ocaml-data-notation-0.0.11 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/odn && make install && cd ..
 
-tar -xvf oasis.tar.gz
-cd oasis && ./configure && make && sudo make install && cd ..
+tar -xvf oasis.tar.gz && cd oasis-0.4.0 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/userconf && rm -rf ~/.opam/4.00.1/lib/oasis && rm -rf ~/.opam/4.00.1/lib/plugin-loader
+make install || sudo cp _build/src/cli/Main.byte /usr/local/bin/oasis && cd ..
 
 echo "Y\n" | opam reinstall eliom
 
-git clone https://github.com/MassD/bson.git
-cd bson && ./configure && make && make install && cd ..
+git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
 
-git clone https://github.com/MassD/mongo.git
-cd mongo && ./configure && make && make install && cd ..
+git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
