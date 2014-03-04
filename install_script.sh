cfc0214852b083b5146e46bf054a5fa6221ea4b7
ArnaudParant <ard.parant@gmail.com>
2014-03-03 20:45:39 -0600

    [Update] install_script.sh, clone a specific version to improve stability of the installation.

--- install_script.sh
+++ install_script.sh
@@ -14,17 +14,34 @@ opam switch 4.00.1
 eval `opam config env`
 
 tar -xvf ocaml-data-notation.tar.gz && cd ocaml-data-notation-0.0.11 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/odn && make install && cd ..
+eval `opam config env`
 
 tar -xvf oasis.tar.gz && cd oasis-0.4.0 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/userconf && rm -rf ~/.opam/4.00.1/lib/oasis && rm -rf ~/.opam/4.00.1/lib/plugin-loader
+eval `opam config env`
 
-echo "Y\n" | opam install dbm menhir eliom yojson
+opam install findlib lwt menhir deriving react ocamlssl calendar optcomp ipaddr ocamlnet pcre-ocaml cryptokit ocaml-text ocamlsqlite3 dbm camlzip yojson
+eval `opam config env`
 
-git clone https://github.com/ocsigen/wikidoc.git && cd wikidoc && make && sudo make uninstall && rm -rf ~/.opam/4.00.1/lib/wikidoc
-make install || sudo make install && cd ..
+# git clone https://github.com/ocsigen/deriving.git && cd deriving && git checkout 91da24ac3c63de8add4dd995bfd80c749b7fbf47 && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/deriving && make install && cd ..
+# eval `opam config env`
+
+git clone https://github.com/ocsigen/js_of_ocaml.git && cd js_of_ocaml && git checkout de165e2f8d4c3a5f0a213ba995b10fc25cdb3099 && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/js_of_ocaml && sudo make install && cd ..
+eval `opam config env`
+
+git clone https://github.com/ocsigen/tyxml.git && cd tyxml && git checkout 54418e879c1edec1c44018f22bb6688c389e3a14 && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/tyxml && make install && cd ..
+eval `opam config env`
+
+git clone https://github.com/ocsigen/ocsigenserver.git && cd ocsigenserver && git checkout 04ecb80fe5f9f340405626e05b5ff4e72c7acc31 && sh configure && make && make uninstall && rm -rf  ~/.opam/4.00.1/lib/ocsigenserver && sudo make install && cd ..
+eval `opam config env`
+
+git clone https://github.com/ocsigen/eliom.git && cd eliom && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/eliom && sudo make install && cd ..
+eval `opam config env`
 
 git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
+eval `opam config env`
 
 git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
+eval `opam config env`
 
 sudo /etc/init.d/mongodb start
 
