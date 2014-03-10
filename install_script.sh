a449d7a1f0a4cc9e898650d7582093062f7702f9
Arnaud Parant <ard.parant@gmail.com>
2014-03-10 18:06:43 +0000

    Update install_script.sh
    Remove impossible getting specific version...

--- install_script.sh
+++ install_script.sh
@@ -19,19 +19,14 @@ eval `opam config env`
 git clone https://github.com/ocsigen/deriving.git && cd deriving && git checkout 91da24ac3c63de8add4dd995bfd80c749b7fbf47 && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/deriving && make install && cd ..
 eval `opam config env`
 
-git clone https://github.com/ocsigen/js_of_ocaml.git && cd js_of_ocaml && git checkout de165e2f8d4c3a5f0a213ba995b10fc25cdb3099 && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/js_of_ocaml && sudo make install && cd ..
-eval `opam config env`
-
 git clone https://github.com/ocsigen/wikidoc.git && cd wikidoc && git checkout 0cbe60d67f363f911ac77ab2c0a611cad5384e91 && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/wikidoc && sudo make install && cd ..
 eval `opam config env`
 
-git clone https://github.com/ocsigen/tyxml.git && cd tyxml && git checkout 54418e879c1edec1c44018f22bb6688c389e3a14 && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/tyxml && make install && cd ..
-eval `opam config env`
-
-opam install ocsigenserver
-eval `opam config env`
-
-git clone https://github.com/ocsigen/eliom.git && cd eliom && git checkout 2107b8bee3772845169d2944cc389f4e7d19b6cd && sh configure && make && make uninstall && rm -rf ~/.opam/4.00.1/lib/eliom && sudo make install && cd ..
+# tyxml 54418e879c1edec1c44018f22bb6688c389e3a14
+# js_of_ocaml 5f45acbe037bd2aa96ee4f828173c7f46f5f5fe0
+# ocsigenserver 04ecb80fe5f9f340405626e05b5ff4e72c7acc31
+# eliom 2107b8bee3772845169d2944cc389f4e7d19b6cd
+opam install tyxml js_of_ocaml ocsigenserver eliom
 eval `opam config env`
 
 git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
