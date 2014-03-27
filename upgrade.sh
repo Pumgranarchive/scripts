23ceb21a4ab0de91533e33dfb7a153f1b58a90ad
Arnaud Parant <ard.parant@gmail.com>
2014-03-27 12:51:40 +0000

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-tar -xvf ocaml-data-notation.tar.gz && cd ocaml-data-notation-0.0.11 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/odn && make install && cd ..
+opam install ocaml-rdf
 
-tar -xvf oasis.tar.gz && cd oasis-0.4.0 && ./configure && make && rm -rf ~/.opam/4.00.1/lib/userconf && rm -rf ~/.opam/4.00.1/lib/oasis && rm -rf ~/.opam/4.00.1/lib/plugin-loader
-make install || sudo cp _build/src/cli/Main.byte /usr/local/bin/oasis && cd ..
 
-git clone https://github.com/ocsigen/wikidoc.git && cd wikidoc && make && rm -rf ~/.opam/4.00.1/lib/wikidoc
-make install || sudo make install && cd ..
-
-echo "Y\n" | opam reinstall eliom
-
-git clone https://github.com/MassD/bson.git && cd bson && ./configure && make && rm -rf ~/.opam/4.00.1/lib/bson && make install && cd ..
-
-git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
