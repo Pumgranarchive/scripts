3558eb0b5f3ad0d14fe520b71170fbc4fe5b0835
Arnaud Parant <ard.parant@gmail.com>
2014-06-29 19:38:50 -0500

    Update upgrade_1.2_to_1.5.sh

--- upgrade/upgrade_1.2_to_1.5.sh
+++ upgrade/upgrade_1.2_to_1.5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
+git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make lwt && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
 eval `opam config env`
 
 sh ../install/4store_install.sh
