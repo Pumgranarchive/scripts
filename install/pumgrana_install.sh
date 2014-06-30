cd0e06d51d659716dff24667c0bdae76db62659e
ArnaudParant <ard.parant@gmail.com>
2014-06-30 17:03:06 -0500

    Added pumgrana's libraries dependencies

--- install/pumgrana_install.sh
+++ install/pumgrana_install.sh
@@ -2,6 +2,8 @@
 
 echo "Pumgrana - instalation..."
 
+(cd /tmp && git clone https://github.com/Pumgrana/libraries.git && cd libraries && make reinstall)
+
 git clone https://github.com/Pumgrana/prototype.git && cd prototype && ./export_db/import.sh && make && cd ..
 
 printf "Done\n\n"
--- upgrade/upgrade_1.2_to_1.5.sh
+++ upgrade/upgrade_1.2_to_1.5.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
+
 git clone https://github.com/MassD/mongo.git && cd mongo && ./configure && make lwt && rm -rf ~/.opam/4.00.1/lib/mongo && make install && cd ..
 eval `opam config env`
+
+(cd /tmp && git clone https://github.com/Pumgrana/libraries.git && cd libraries && make reinstall)
 
 sh ../install/4store_install.sh
