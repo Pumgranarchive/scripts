05f866ceeaf7694f122cdaaa941714c8ed06db41
ArnaudParant <ard.parant@gmail.com>
2014-07-14 02:48:42 -0500

    Added reinit + Improve run

--- pumgrana/run.sh
+++ pumgrana/run.sh
@@ -2,7 +2,7 @@
 
 sudo /etc/init.d/mongodb start
 
-sh 4store/killDaemon.sh
-sh 4store/run.sh
+sh ../scripts/4store/killDaemon.sh
+sh ../scripts/4store/run.sh
 
-(cd prototype && make test.byte &)
+make test.byte &
