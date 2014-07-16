10e79cc102701f938f4df133a49d808a73e5ac68
alex.saddour@gmail.com <alex.saddour@gmail.com>
2014-07-16 16:00:41 -0400

    EIP_VM: EDIT: init_aptitude and opam_install

--- install/init_aptitude.sh
+++ install/init_aptitude.sh
@@ -8,4 +8,10 @@ NEW_FILE=eip_vm_files/sources.list
 sudo cp -v $ORIGINAL_FILE $BACKUP_FILE
 sudo cp -v $NEW_FILE $ORIGINAL_FILE
 
+sudo aptitude install debian-keyring debian-archive-keyring
+
+sudo apt-get update
+
 echo "...Done"
+
+
