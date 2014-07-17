8c2776d8e4080218dc200ecb4dc1dd6df98d9712
alex.saddour@gmail.com <alex.saddour@gmail.com>
2014-07-16 20:33:16 -0400

    EIP_VM: EDIT 4store mongodb pumgrana

--- install/4store_install.sh
+++ install/4store_install.sh
@@ -2,7 +2,7 @@
 
 echo "4Store - installation..."
 
-sudo apt-get install build-essential libpcre3-dev librasqal3-dev libtool libraptor1-dev libglib2.0-dev ncurses-dev libreadline-dev autoconf libuuid1 uuid uuid-dev
+sudo apt-get install build-essential libpcre3-dev libglib2.0-dev ncurses-dev libreadline-dev autoconf libuuid1 uuid uuid-dev libtool librasqal3-dev libraptor1-dev 
 
 (cd /tmp/ && git clone https://github.com/garlik/4store.git && cd 4store && ./autogen.sh && ./configure && make && sudo make install)
 
@@ -10,10 +10,15 @@ sudo apt-get install build-essential libpcre3-dev librasqal3-dev libtool librapt
 
 (cd /tmp/ && git clone https://github.com/mirage/ocaml-conduit.git && cd ocaml-conduit && git checkout 944960ae9a7698fe90474b0c9d686f0b6f0d8baf && make && make install)
 
+opam install uri
+opam install fieldslib
+
 git clone https://github.com/mirage/ocaml-cohttp.git && cd ocaml-cohttp && oasis setup && make
-sudo make uninstall
-sudo make install
+make uninstall
+make install
 cd ..
+
+opam install ulex xmlm uuidm
 
 git clone https://github.com/zoggy/ocaml-rdf.git && cd ocaml-rdf && ./configure && make
 make uninstall
--- install/pumgrana_install.sh
+++ install/pumgrana_install.sh
@@ -4,6 +4,12 @@ echo "Pumgrana - instalation..."
 
 (cd /tmp && git clone https://github.com/Pumgrana/libraries.git && cd libraries && make reinstall)
 
-git clone https://github.com/Pumgrana/prototype.git && cd prototype && ./export_db/import.sh && make && cd ..
+sudo apt-get install curl
+
+
+git clone https://github.com/Pumgrana/prototype.git && cd prototype && make && cd ..
+
+(cd prototype ; sudo ../../mongo/import.sh ; sudo ../../4store/run.sh;sudo ../../4store/import_db.sh)
+
 
 printf "Done\n\n"
