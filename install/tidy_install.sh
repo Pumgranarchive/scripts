43180a74049a0106d4d7edb27fa0b8f7216a559e
ArnaudParant <ard.parant@gmail.com>
2015-03-17 20:58:25 +0100

    [Install] [Tidy] Fixed

--- install/tidy_install.sh
+++ install/tidy_install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo -e "y\n" | sudo apt-get install xsltproc make cmake
+echo -e "y\n" | sudo apt-get install xsltproc make cmake gcc g++
 
-(cd /tmp/ && git clone https://github.com/w3c/tidy-html5.git && cd tidy-html5 && cmake . && make && make install)
+export CMAKE_C_COMPILER=/usr/bin/gcc
+export CMAKE_CXX_COMPILER=/usr/bin/g++
+echo -e "# C / C++ setting\nexport CMAKE_C_COMPILER=/usr/bin/gcc\nexport CMAKE_CXX_COMPILER=/usr/bin/g++" >> ~/.bashrc
+
+cd /tmp/
+git clone https://github.com/w3c/tidy-html5.git
+cd tidy-html5
+git checkout c013fa97d9fd7ba4257eda6c967c7b1cd9a453d8
+cmake .
+make
+sudo make install
+sudo ln -s /usr/local/bin/tidy5 /usr/local/bin/tidy
+cd -
